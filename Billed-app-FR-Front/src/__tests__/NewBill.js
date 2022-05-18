6740f90d645c21c14b5f6cb9555ce3f01a7c2cbc
Valentin Pierre <valentin.pierre71@gmail.com>
2022-05-18 17:08:12 +0200

    Test unitaire NewBills chargement d'une image

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -7,6 +7,8 @@ import NewBill from "../containers/NewBill.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
+import store from "../__mocks__/store.js";
+import userEvent from "@testing-library/user-event";
 
 
 describe("Given I am connected as an employee", () => {
@@ -27,6 +29,29 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
       expect(windowIcon.classList.contains('active-icon')).toBe(true)
 
+    })
+
+    describe("When i click on Choisir un fichier", () => {
+      test("Then an image should be added", () => {
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        const html = NewBillUI()
+        document.body.innerHTML = html
+        const onNavigate = (pathname) => {document.body.innerHTML = ROUTES({pathname})}
+
+        const currentNewBill = new NewBill({document, onNavigate, store: store, localStorage: localStorage})
+        const fileTest = new File(['hello'], 'hello.png', {type: 'image/png'})
+
+        const handleChangeFile = jest.fn((e) => currentNewBill.handleChangeFile(e))
+        
+        const selectFile = screen.getByTestId("file")
+        selectFile.addEventListener('change', handleChangeFile)
+        userEvent.upload(selectFile, fileTest)
+        expect(selectFile.files[0]).toStrictEqual(fileTest)
+        expect(selectFile.files.item(0)).toStrictEqual(fileTest)
+        expect(selectFile.files).toHaveLength(1)
+      })
     })
   })
 })
